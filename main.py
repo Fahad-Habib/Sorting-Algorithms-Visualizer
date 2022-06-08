db3f6c43433a1cc672b9dc139e00730b25fa902e
Fahad-Habib <mr.fahadhabib@gmail.com>
2022-06-08 13:10:14 +0500

    main

---
+++ main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file('my.kv')
+
+
+class SAVApp(App):
+    def build(self):
+        Window.clearcolor = (0, 0, 0, 1)
+        return kv
+
+
+if __name__ == '__main__':
+    SAVApp().run()
